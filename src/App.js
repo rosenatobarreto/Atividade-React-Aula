05d89c03878588545bca204e47a0f36c682dd1f4
rosenatobarreto <rosenatoblima@gmail.com>
2022-05-10 00:12:02 -0300

    Atividade Sala de Aula com React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  state = {
+    nomePacote: "",
+    epoca: "",
+    preco: 0,
+    periodoEmDias: 0
+  }
+
+  create = () => {
+   console.log(this.state);
+  }
+
+
+  render () {
+    
+    return (
+      <div className="Container">
+      <div className="App">
+        <h1>Cadastro de Pacotes</h1>
+      
+        <label className="Field">Pacote</label>
+        <input type="text" className="Input-field" value={this.state.nomePacote} onChange={(e) => {this.setState({nomePacote: e.target.value})}} />
+        <br/>
+        <label className="Field">Época</label>
+        <input type="text" className="Input-field" value={this.state.epoca} onChange={(e) => {this.setState({epoca: e.target.value})}} />
+        <br/>
+        <label className="Field">Preço</label>
+        <input type="text" className="Input-field" value={this.state.preco} onChange={(e) => {this.setState({preco: e.target.value})}} />
+        <br/>
+        <label className="Field">Período</label>
+        <input type="text" className="Input-field" value={this.state.periodo} onChange={(e) => {this.setState({periodo: e.target.value})}} />
+
+        <br/>
+        <button className="Button" onClick={this.create}>Salvar</button>
+
+       
+      </div>
+      
+  
+      </div>
+    );
+  }
+
 }
 
-export default App;
+
+
+
